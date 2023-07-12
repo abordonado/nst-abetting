0f86b12af70b9a99d1ba4c9aab4b72ef9f4cd7aa
Alberto Bordonado <alberto.bordonado@gmail.com>
2023-07-13 00:11:14 +0200

    Update configuration with ApiSports environment variables

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,6 +4,11 @@ export default registerAs('config', () => {
   return {
     environment: process.env.ENV,
 
+    apiSports: {
+      key: process.env.API_SPORTS_KEY,
+      url: process.env.API_SPORTS_URL,
+    },
+
     host: {
       port: process.env.PORT,
     },
